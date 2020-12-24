db8a95127dcdb515e11cb2f29a52c1fd483446d6
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-12-25 02:13:19 +0530

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:HeyThere/screens/welcome_screen.dart';
 import 'package:HeyThere/screens/login_screen.dart';
 import 'package:HeyThere/screens/registration_screen.dart';
 import 'package:HeyThere/screens/chat_screen.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/welcome_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
