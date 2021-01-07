1e13173b7a6443369e93c046b785d4649efdd4ef
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2021-01-08 00:26:12 +0530

    Main updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,16 @@ import 'package:HeyThere/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/welcome_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-   runApp(FlashChat());
+  runApp(FlashChat());
 }
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return 
-    MaterialApp(
+    return MaterialApp(
       // theme: ThemeData.dark().copyWith(
       //   textTheme: TextTheme(
       //     bodyText2: TextStyle(color: Colors.black54),
